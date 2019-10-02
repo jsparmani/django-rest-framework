5a48304f4d91d9363727bded30499987794d6fb4
Jay Parmani <jsparmani@gmail.com>
2019-10-02 13:54:55 +0530

    get only assigned tags and ingredients

--- recipe/views.py
+++ recipe/views.py
@@ -18,7 +18,14 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet, mixins.CreateMo
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
 
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(self.request.query_params.get('assigned_only'))
+
+        queryset = self.queryset
+
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
         """Create a new tag"""
@@ -37,7 +44,14 @@ class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet, mixins.C
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
 
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(self.request.query_params.get('assigned_only'))
+
+        queryset = self.queryset
+
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
         """Create a new ingredient"""
